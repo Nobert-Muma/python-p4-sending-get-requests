be2160bc7238098993857353be49e12c09283160
Nobert-Muma <mumanobert17@gmail.com>
2024-10-05 23:04:44 +0300

    Sending GET Requests

--- lib/get.py
+++ lib/get.py
@@ -8,3 +8,8 @@ response = requests.get(url)
 json_content = json.loads(response.text)
 
 print(json.dumps(json_content, indent=4))
+
+# import requests
+# url='https://learn-co-curriculum.github.io/json-site-example/'
+# response=requests.get(url)
+# print(response.text)
